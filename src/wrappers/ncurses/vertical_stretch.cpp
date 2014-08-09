8c366ab7d62e90a1c6a6477df3f8ccbeb4a37cff
fro_ozen <fro_ozen@gmx.de>
2014-08-09 16:15:19 +0200

    Modify ncurses::Vertical_stretch_layout to use complete height
    Some of the height was lost trough integer division

--- src/wrappers/ncurses/vertical_stretch.cpp
+++ src/wrappers/ncurses/vertical_stretch.cpp
@@ -1,4 +1,5 @@
 #include "vertical_stretch.h"
+#include <algorithm>
 
 namespace ncurses
 {
@@ -6,10 +7,15 @@ namespace ncurses
             char key, std::shared_ptr < Window > window )
     {
         int height = window->get_height () / ( ( int ) elements.size () );
+        int rest = window->get_height () % ( ( int ) elements.size () );
         for ( int i = 0; i < elements.size (); i++ )
         {
-            std::shared_ptr < Window > element_window = std::make_shared < Window >
-                ( window->get_x (), window->get_y () + height * i, window->get_width (), height );
+            int x = window->get_x ();
+            int y = window->get_y () + height * i + std::min ( i, rest );
+            int width = window->get_width ();
+            int win_height = height + ( i < rest ? 1 : 0 );
+
+            std::shared_ptr < Window > element_window = std::make_shared < Window > ( x, y, width, win_height );
             elements.at ( i )->update ( key, element_window );
         }
     }
