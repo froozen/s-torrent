7df8c123d2ee18f77f4285fbe3f436bd354527ae
fro_ozen <fro_ozen@gmx.de>
2014-08-16 15:21:36 +0200

    Split utils::Configuration and utils::Configuration_element

--- src/utils/configuration.h
+++ src/utils/configuration.h
@@ -3,7 +3,7 @@
 
 #include <jsoncpp/json/json.h>
 #include <memory>
-#include <string>
+#include "configuration_element.h"
 
 namespace utils
 {
@@ -24,38 +24,6 @@ namespace utils
             static std::shared_ptr < Json::Value > json_value;
     };
 
-    class Configuration_element
-    {
-        public:
-            Configuration_element ();
-            Configuration_element ( Json::Value* root );
-
-            std::string to_string ();
-
-            bool get_bool ( std::string identifier );
-            int get_int ( std::string identifier );
-            std::string get_string ( std::string identifier );
-            double get_double ( std::string identifier );
-            Configuration_element get_element ( std::string identifier );
-
-            void set_bool ( std::string identifier, bool new_value );
-            void set_int  ( std::string identifier, int new_value );
-            void set_string ( std::string identifier, std::string new_value );
-            void set_double ( std::string identifier, double new_value );
-            void set_element ( std::string identifier, const Configuration_element& new_value );
-
-            virtual ~Configuration_element ();
-
-        private:
-            // Values returned for if something went wrong
-            const bool          DEFAULT_BOOL    = false;
-            const int           DEFAULT_INT     = -1;
-            const std::string   DEFAULT_STRING  = "None";
-            const double        DEFAULT_DOUBLE  = -1;
-
-            Json::Value* value;
-            bool own;
-    };
 }
 
 #endif //CONFIG_GUARD
